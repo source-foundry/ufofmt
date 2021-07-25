d6cea4c392a528869a9c9a223344020e4f73f118
Chris Simpkins <git.simpkins@gmail.com>
2021-07-25 16:19:14 -0400

    [errors] add test

--- src/lib/errors.rs
+++ src/lib/errors.rs
@@ -9,3 +9,15 @@ pub(crate) fn print_error(err: &UfofmtError) {
     let error_indicator = "[ERROR]".red().bold();
     let _ = writeln!(stderr(), "{} {}", error_indicator, err);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ufofmterror_cast() {
+        let custom_err = std::io::Error::new(std::io::ErrorKind::Other, "test error");
+        let ufe = UfofmtError::from(custom_err);
+        assert_eq!(ufe.to_string(), "test error");
+    }
+}
