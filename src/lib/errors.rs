400eccfa5f64bafe39e30351f19cff9fc1756f49
Chris Simpkins <chris@sourcefoundry.org>
2021-07-29 10:24:42 -0400

    add new custom error type

--- src/lib/errors.rs
+++ src/lib/errors.rs
@@ -1,18 +1,52 @@
-use std::io::{stderr, Write};
+use std::fmt;
 
 use colored::*;
 use lazy_static::lazy_static;
 
-pub type UfofmtError = Box<dyn std::error::Error + Send + Sync + 'static>;
-pub type Result<T> = std::result::Result<T, UfofmtError>;
+pub(crate) type Result<T> = std::result::Result<T, UfofmtError>;
 
 lazy_static! {
     pub static ref ERROR_INDICATOR: ColoredString = "[ERROR]".red().bold();
     pub static ref OK_INDICATOR: ColoredString = "[OK]".green().bold();
 }
 
-pub(crate) fn print_error(err: &UfofmtError) {
-    let _ = writeln!(stderr(), "{} {}", *ERROR_INDICATOR, err);
+// ufofmt custom error type
+#[derive(Debug)]
+pub(crate) struct UfofmtError {
+    pub(crate) message: String,
+    pub(crate) kind: UfofmtErrorKind,
+}
+
+impl fmt::Display for UfofmtError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> std::result::Result<(), fmt::Error> {
+        match &self.kind {
+            UfofmtErrorKind::Read => {
+                write!(f, "read error: {}", &self.message)
+            }
+            UfofmtErrorKind::Write => {
+                write!(f, "write error: {}", &self.message)
+            }
+            UfofmtErrorKind::InvalidPath => {
+                write!(f, "invalid path error: {}", &self.message)
+            }
+        }
+    }
+}
+
+impl std::error::Error for UfofmtError {}
+
+impl UfofmtError {
+    // Associated functions
+    pub(crate) fn new(kind: UfofmtErrorKind, message: &str) -> Self {
+        Self { kind, message: message.to_string() }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub(crate) enum UfofmtErrorKind {
+    InvalidPath,
+    Read,
+    Write,
 }
 
 #[cfg(test)]
@@ -20,9 +54,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_ufofmterror_cast() {
-        let custom_err = std::io::Error::new(std::io::ErrorKind::Other, "test error");
-        let ufe = UfofmtError::from(custom_err);
-        assert_eq!(ufe.to_string(), "test error");
+    fn test_ufofmterror_invalid_path() {
+        let ufe = UfofmtError::new(UfofmtErrorKind::InvalidPath, "testpath.ufo was not found");
+        assert_eq!(ufe.to_string(), "invalid path error: testpath.ufo was not found");
+    }
+
+    #[test]
+    fn test_ufofmterror_read() {
+        let ufe = UfofmtError::new(UfofmtErrorKind::Read, "testpath.ufo");
+        assert_eq!(ufe.to_string(), "read error: testpath.ufo");
+    }
+
+    #[test]
+    fn test_ufofmterror_write() {
+        let ufe = UfofmtError::new(UfofmtErrorKind::Write, "testpath.ufo");
+        assert_eq!(ufe.to_string(), "write error: testpath.ufo");
     }
 }
