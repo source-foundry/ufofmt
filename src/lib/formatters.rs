814b18f610226e65bf803c46423a8bc7394e398d
Chris Simpkins <git.simpkins@gmail.com>
2021-08-25 22:48:16 -0400

    [formatters] replace glif test

--- src/lib/formatters.rs
+++ src/lib/formatters.rs
@@ -156,10 +156,10 @@ mod tests {
         let expected_fontinfo_path = Path::new("testdata/expected/fontinfo.default.plist");
         let expected_fontinfo_string = fs::read_to_string(expected_fontinfo_path).unwrap();
         // glif file
-        // let test_glyph_string =
-        //     fs::read_to_string(&test_ufo_path.join("glyphs").join("A_.glif")).unwrap();
-        // let expected_glyph_path = Path::new("testdata/expected/A_.default.glif");
-        // let expected_glyph_string = fs::read_to_string(expected_glyph_path).unwrap();
+        let test_glyph_string =
+            fs::read_to_string(&test_ufo_path.join("glyphs").join("A_.glif")).unwrap();
+        let expected_glyph_path = Path::new("testdata/expected/A_.default.glif");
+        let expected_glyph_string = fs::read_to_string(expected_glyph_path).unwrap();
         // groups.plist
         let test_groups_string = fs::read_to_string(&test_ufo_path.join("groups.plist")).unwrap();
         let expected_groups_path = Path::new("testdata/expected/groups.default.plist");
@@ -188,7 +188,7 @@ mod tests {
         let expected_contents_string = fs::read_to_string(expected_contents_path).unwrap();
 
         // observed vs. expected string tests
-        // assert_eq!(expected_glyph_string, test_glyph_string);
+        assert_eq!(expected_glyph_string, test_glyph_string);
         assert_eq!(expected_fontinfo_string, test_fontinfo_string);
         assert_eq!(expected_groups_string, test_groups_string);
         assert_eq!(expected_kerning_string, test_kerning_string);
